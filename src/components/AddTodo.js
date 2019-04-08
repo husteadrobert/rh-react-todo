6813c61427101d9fe0a20d72476f4fe63b8fa542
Robert Hustead <robert.hustead@favy.co.jp>
2019-04-08 22:43:20 +0900

    Add modal

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import AddTodoModal from './addtodomodal';
 
 class AddTodo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      error: undefined
+      error: undefined,
+      modal_active: false
     };
     this.handleAddTodo = this.handleAddTodo.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
   handleAddTodo(e) {
     e.preventDefault();
@@ -14,18 +17,22 @@ class AddTodo extends React.Component {
     const error = this.props.handleAddTodo(todo);
 
     this.setState(() => ({error}));
+    this.setState(() => ({modal_active: false}));
 
     if (!error) {
       e.target.elements.todo.value = '';
     }
   }
+  handleClose(){
+    this.setState(() => ({ modal_active: false }));
+  }
   render() {
     return(
       <div>
-        <form onSubmit={this.handleAddTodo}>
-          <input type="text" name="todo" />
-          <button>Add Todo</button>    
-        </form>
+        <button onClick={(e) => { 
+          this.setState(() => ({modal_active: true}))
+        }}>Add Todo</button>
+        <AddTodoModal modal_active={this.state.modal_active} handleClose={this.handleClose} handleAddTodo={this.handleAddTodo}/>
       </div>
     );
   }
---
+++ src/components/AddTodoModal.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+const AddTodoModal = (props) => (
+  <Modal
+    isOpen={props.modal_active}
+    onRequestClose={props.handleClose}
+    appElement={document.getElementById('app')}
+    contentLabel="Add Todo"
+  >
+    <h1>Lol Modal</h1>
+    <form onSubmit={props.handleAddTodo}>
+      <input type="text" name="todo" />
+      <button>Add Todo</button>    
+    </form>
+  </Modal>
+);
+
+export default AddTodoModal;
